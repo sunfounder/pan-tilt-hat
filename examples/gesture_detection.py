45f8df8639c9aacaa9a8212eb7d7b234c9f7bb38
dino <845864704@qq.com>
2021-10-20 13:35:15 +0800

    add set model and set labels for image classification and objects detection

--- examples/gesture_detection.py
+++ examples/gesture_detection.py
@@ -2,7 +2,7 @@ from vilib import Vilib
 
 def main():
     Vilib.camera_start(inverted_flag=True)
-    Vilib.display(imshow=True,web=True)
+    Vilib.display(local=True,web=True)
     Vilib.gesture_detect_switch(True)
 
 if __name__ == "__main__":
