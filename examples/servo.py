6f72fc862700fe238d8d0c4ad1ab127b39c8360a
Lovmong <1967471283@qq.com>
2022-05-25 17:13:51 +0800

    Use  the pigpio library to drive the servo

---
+++ examples/servo.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import pigpio
+from time import sleep
+# Start the pigpiod daemon
+import subprocess
+p = subprocess.Popen('sudo pigpiod', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+# result = p.stdout.read().decode('utf-8')
+# status = p.poll()
+# print(status,result)
+
+# AttributeError
+'''
+> Use the DMA PWM of the pigpio library to drive the servo
+> Map the servo angle (0 ~ 180 degree) to (-90 ~ 90 degree)
+
+'''
+
+class Servo():
+    MAX_PW = 1250  # 0.5/20*100
+    MIN_PW = 250 # 2.5/20*100
+    _freq = 50 # 50 Hz, 20ms
+ 
+    def __init__(self, pin, min_angle=-90, max_angle=90):
+
+        self.pi = pigpio.pi()
+        self.pin = pin 
+        self.pi.set_PWM_frequency(self.pin, self._freq)
+        self.pi.set_PWM_range(self.pin, 10000)      
+        self.angle = 0
+        self.max_angle = max_angle
+        self.min_angle = min_angle
+        self.pi.set_PWM_dutycycle(self.pin, 0)
+
+    def set_angle(self, angle):
+        if angle > self.max_angle:
+            angle = self.max_angle
+        elif angle < self.min_angle:
+            angle = self.min_angle
+        self.angle = angle
+        duty = self.map(angle, -90, 90, 250, 1250)
+        self.pi.set_PWM_dutycycle(self.pin, duty)
+
+
+    def get_angle(self):
+        return self.angle
+
+    # will be called automatically when the object is deleted
+    # def __del__(self):
+    #     pass
+
+    def map(self, x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+if __name__ =='__main__':
+    from vilib import Vilib
+    Vilib.camera_start(vflip=True,hflip=True) 
+    Vilib.display(local=True,web=True)
+
+    pan = Servo(pin=13, max_angle=90, min_angle=-90)
+    tilt = Servo(pin=12, max_angle=30, min_angle=-90)
+    panAngle = 0
+    tiltAngle = 0
+    pan.set_angle(panAngle)
+    tilt.set_angle(tiltAngle)
+    sleep(1)
+
+    while True:
+        for angle in range(0, 90, 1):
+            pan.set_angle(angle)
+            tilt.set_angle(angle)
+            sleep(.01)
+        sleep(.5)
+        for angle in range(90, -90, -1):
+            pan.set_angle(angle)
+            tilt.set_angle(angle)
+            sleep(.01)
+        sleep(.5)
+        for angle in range(-90, 0, 1):
+            pan.set_angle(angle)
+            tilt.set_angle(angle)
+            sleep(.01)
+        sleep(.5)
+
